f43a368019ea991f67ae71fb60dca3b3049e155f
Daniel Benamy <dbenamy@prod-www-varnish1.(none)>
2012-01-12 19:32:17 -0500

    Fix varnish_health.sh.

--- cloudkick-plugins/varnish_health.sh
+++ cloudkick-plugins/varnish_health.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# TODO merge this into cloudkick's varnish plugin and send them a pull request.
+
 set -o errexit
 set -o nounset
 set -o pipefail
 
 health=$(varnishadm -T localhost:6082 debug.health | egrep "^Backend ")
-sick=$(echo $health | grep Sick)
+sick=$(echo $health | grep Sick) || true
 if [[ "$sick" == *Sick* ]]; then
     echo "status err $sick"
+else
+    echo "status ok ok"
 fi
-echo $health | sed 's/Healthy/1/' | sed 's/Sick/0/' | awk '{ print "metric " $2 "_healthy int " $4 }'
+echo "$health" | sed 's/Healthy/1/g;s/Sick/0/g' | awk '{ print "metric " $2 "_healthy int " $4 }'
